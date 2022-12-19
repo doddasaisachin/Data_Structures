578fb6f2df288d20881df36bd30c62708ec7ea8c
doddasaisachin <115567565+doddasaisachin@users.noreply.github.com>
2022-12-19 11:02:58 +0530

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,26 +1,17 @@
-
-def create_stack():
-    stack = []
-    return stack
-    
-def check_empty(stack):
-    return len(stack) == 0
-
-def push(stack, item):
-    stack.append(item)
-    print("pushed item: " + item)
-
-def pop(stack):
-    if (check_empty(stack)):
-        return "stack is empty"
-
-    return stack.pop()
-
-
-stack = create_stack()
-push(stack, str(1))
-push(stack, str(2))
-push(stack, str(3))
-push(stack, str(4))
-print("popped item: " + pop(stack))
-print("stack after popping an element: " + str(stack))
+class Stack:
+    def create_empty_stack(self,stack):
+        self.stack=[]
+    def display(self,stack):
+        return self.stack
+    def push(self,stack,element):
+        self.stack.append(element)
+    def pop(self,stack):
+        self.stack.pop()
+    def isempty(self,stack):
+        if len(self.stack)==0:
+            return 'TRUE'
+        else:
+            return 'FALSE'
+    def peek(self,stack):
+        top=-1
+        return self.stack[top]

3fb76aa99bbedba62f2b4397df7a065783a43077
doddasaisachin <115567565+doddasaisachin@users.noreply.github.com>
2022-12-19 12:00:21 +0530

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -1,34 +1,36 @@
-
 class Queue:
-
+    
     def __init__(self):
-        self.queue = []
-
-    def enqueue(self, item):
-        self.queue.append(item)
+        self.queue=[]
+        self.front=0
+        self.rear=-1
+        
+    def enqueue(self,element):
+        self.queue.insert(self.front,element)
         
     def dequeue(self):
-        if len(self.queue) < 1:
-            return None
-        return self.queue.pop(0)
-
+        if len(self.queue)>0:
+            self.queue.pop()
+        else:
+            return 'Queue is empty'
+        
     def display(self):
-        print(self.queue)
-
+        return self.queue
+    
     def size(self):
         return len(self.queue)
-
-
-q = Queue()
-q.enqueue(1)
-q.enqueue(2)
-q.enqueue(3)
-q.enqueue(4)
-q.enqueue(5)
-
-q.display()
-
-q.dequeue()
-
-print("After removing an element")
-q.display()
+    
+    def last_element(self):
+        return self.queue[self.rear]
+    
+    def front_element(self):
+        return self.queue[self.front]
+    
+    def is_empty(self):
+        if len(self.queue)==0:
+            return 'TRUE'
+        else:
+            return 'False'
+        
+    def peek(self):
+        return self.queue[self.front]
